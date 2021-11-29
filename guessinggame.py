f35fb0aaf4012f3e84f5d11a2c873178c294bd2e
Collins Ezedike-egwom <62267326+Collins-github@users.noreply.github.com>
2021-11-29 11:54:13 +0100

    Update guessinggame.py
    I've updated the Guessing Game with a single change. When the game ends, whether by a win or a lose (user is out of guesses), there is a prompt to play again. I did this with a while loop and an if syntax I borrowed from Big Book Of Small Python Projects by Ai Sweigart.

--- guessinggame.py
+++ guessinggame.py
@@ -1,25 +1,33 @@
 import random
-
-print('''Try to guess a number between 0 and 9.
-
-Hint: 'Too hot!' means your guess was greater than the secret number. 'Too cold!' means it was lesser than the secret number.
+while True:
+	print('''
+Try to guess a number between 0 and 9.
+		
+Hint: 
+'Too hot!' means your guess was greater than the secret number. 
+'Too cold!' means it was lesser than the secret number.
 ''')
-
-secret_number = random.randint(0,9)
-available_guesses = 3
-
-while available_guesses != 0:
-	try:
-		guess = int(input("Guess the secret number: "))
-		available_guesses -= 1
-		if guess == secret_number:
-			print("You guessed the number. You win!")
-			break
-		elif guess < secret_number:
-			print("Too cold!")
-		elif guess > secret_number:
-			print("Too hot!")
-	except ValueError:
-		print("Wrong Input!")
-else:
-	print("You are out of guesses. You lose!")
+		
+	secret_number = random.randint(0,9)
+	available_guesses = 3
+		
+	while available_guesses != 0:
+			try:
+				guess = int(input("Guess the secret number: "))
+				available_guesses -= 1
+				if guess == secret_number:
+					print("You guessed the number. You win!")
+					break
+				elif guess < secret_number:
+					print("Too cold!")
+				elif guess > secret_number:
+					print("Too hot!")
+			except ValueError:
+				print("Wrong Input!")
+	else:
+		print("You are out of guesses. You lose!")
+		print(f"The secret number was {secret_number}")
+	print("\nWould you like to play again?(yes/no)")
+	if not input("> ").lower().startswith("y"):
+		break
+print("Thanks for playing!")
